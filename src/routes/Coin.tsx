f630e6f95d7fe7871679e52a782a64a76e1ce8b9
이영우 <zestlee1106@gmail.com>
2023-02-21 10:35:10 +0900

    코인 페이지 추가

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,12 +1,54 @@
-import { useParams } from "react-router-dom";
+import { useState } from "react";
+import { useLocation, useParams } from "react-router-dom";
+import styled from "styled-components";
 
 interface RouteParams {
   coinId: string;
 }
 
-function Coin() {
-  const { coinId } = useParams<RouteParams>();
-  return <h1>Coin: {coinId}</h1>;
+interface RouteState {
+  name: string;
 }
+
+function Coin() {
+  const [loading, setLoading] = useState(true);
+  const { coinId } = useParams<RouteParams>();
+  const {
+    state: { name },
+  } = useLocation<RouteState>();
+  console.log(name);
+
+  return (
+    <Container>
+      <Header>
+        <Title>코인 {coinId}</Title>
+      </Header>
+      {loading ? <Loader>Loading...</Loader> : null}
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  padding: 0px 20px;
+  max-width: 480px;
+  margin: 0 auto;
+`;
+
+const Header = styled.header`
+  height: 10vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Title = styled.h1`
+  font-size: 48px;
+  color: ${(props) => props.theme.accentColor};
+`;
+
+const Loader = styled.span`
+  text-align: center;
+  display: block;
+`;
 
 export default Coin;
